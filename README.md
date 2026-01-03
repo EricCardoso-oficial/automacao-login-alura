aab0a9391dfafe0102c52d9d0f8585dbfde60ea4
EricCardoso-oficial <ericjc16oficial@gmail.com>
2026-01-03 13:07:53 -0300

    Projeto completo

---
+++ README.md
@@ -0,0 +1,47 @@
+# Automação na plataforma do Alura
+
+## Descrição
+Projeto de automação de login na plataforma do Alura utilizando Python(POO) e Selenium WebDriver.
+
+## Instruções de instalação
+```bash
+# Clonar tudo o que estiver no repositório
+git clone https://github.com/EricCardoso-oficial/automacao-login-alura.git
+
+# Acessar a pasta do projeto
+cd automacao-login-alura
+
+# Criar o ambiente virtual
+python -m venv venv
+
+# Ativar o ambiente virtual
+
+# Windows
+venv\Scripts\activate
+
+# Linux, macOS
+source venv/bin/activate
+
+# Instalar todas as dependências do requirements.txt
+pip install -r requirements.txt
+```
+
+## Instruções de uso
+Renomeie o arquivo .env-example para .env e preencha os campos correspondentes.
+
+```bash
+# Acessar pasta src
+cd src
+
+# Executar
+
+# Windows
+python main.py
+
+# Linux, macOS
+python3 main.py
+```
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -10,30 +10,29 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import (NoSuchDriverException,
-SessionNotCreatedException, WebDriverException, TimeoutException, 
-NoSuchElementException, ElementClickInterceptedException,
+SessionNotCreatedException,TimeoutException, NoSuchElementException, 
 ElementNotInteractableException)
 
 TEMPO = 20
 URL_ALURA_LOGIN = ("https://cursos.alura.com.br/loginForm")
 
 class LoginAlura:
-    def __init__(self, email: str, senha: str):
-        self.email = email
-        self.senha = senha
+    def __init__(self):
+        pass
     
     def configuracao_driver(self):
         try:
             # Configura as opções
-            self.opcoes = ChromeOptions()
+            opcoes = ChromeOptions()
+            opcoes.add_experimental_option('detach', True)
             
             # Configura o serviço
-            self.servico = Service(ChromeDriverManager().install())
+            servico = Service(ChromeDriverManager().install())
             
             # Configura o driver
             self.driver = webdriver.Chrome(
-                options=self.opcoes,
-                service=self.servico)
+                options=opcoes,
+                service=servico)
         
         except (NoSuchDriverException, SessionNotCreatedException):
             print(
@@ -50,7 +49,7 @@ class LoginAlura:
 
         try:
             # Procurar o botão de Logar com o Google
-            self.botao_logar_google = esperar_elemento(
+            botao_logar_google = esperar_elemento(
                 driver=self.driver, 
                 tempo=TEMPO, 
                 chave=By.CLASS_NAME, 
@@ -58,13 +57,13 @@ class LoginAlura:
             )
             
             # Clicar no botão
-            self.botao_logar_google.click()
+            botao_logar_google.click()
             sleep(8)
 
             # Procurar o campo para inserir o Gmail
             self.driver.switch_to.window(self.driver.window_handles[-1])
         
-            self.campo_gmail = esperar_elemento(
+            campo_gmail = esperar_elemento(
                 driver=self.driver,
                 tempo=TEMPO,
                 chave=By.ID,
@@ -78,14 +77,14 @@ class LoginAlura:
             gmail_usuario = os.getenv("GMAIL")
             
             # Inserir Gmail
-            self.campo_gmail.send_keys(gmail_usuario)
+            campo_gmail.send_keys(gmail_usuario)
             
             # Pressionar o Enter
-            self.campo_gmail.send_keys(Keys.ENTER)
-            sleep(8)
+            campo_gmail.send_keys(Keys.ENTER)
+            sleep(9)
 
             # Procurar o campo para inserir a Senha
-            self.campo_senha = esperar_elemento(
+            campo_senha = esperar_elemento(
                 driver=self.driver,
                 tempo=TEMPO,
                 chave=By.NAME,
@@ -96,14 +95,15 @@ class LoginAlura:
             senha_usuario = os.getenv("SENHA_GMAIL")
             
             # Inserir a senha
-            self.campo_senha.send_keys(senha_usuario)
+            campo_senha.send_keys(senha_usuario)
             
             # Pressionar Enter
-            self.campo_senha.send_keys(Keys.ENTER)
-            sleep(10)
+            campo_senha.send_keys(Keys.ENTER)
+            sleep(13)
 
+            # Pressionar a tecla Esc para sair da tela de fazer login no Chrome
             pag.press("Esc")
-            sleep(8)
+            sleep(9)
 
         except NoSuchElementException:
             print("Elemento não encontrado")
@@ -116,3 +116,13 @@ class LoginAlura:
         except TimeoutException:
             print("O tempo esgotou antes da condição ser atendida")
             raise
+
+def executar():
+    login_alura = LoginAlura()
+    
+    login_alura.configuracao_driver()
+    login_alura.logar()
+
+# Executar
+executar()
+
